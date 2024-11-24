9bdf85cf475eaf4a5d0b6be75a78d04b48e81881
taskschd1145 <taskschd@hotmail.com>
2024-11-24 14:02:56 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -47,6 +47,7 @@ import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from 
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
+import { CodeRain } from /public/images/icon/CodeRain.png
 import siteLinks from "@/assets/siteLinks.json";
 
 const store = mainStore();
@@ -70,6 +71,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
+  RainCode,
 };
 
 // 链接跳转
